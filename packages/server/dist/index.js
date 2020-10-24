95b062b82eea2189af79920bcc5b79b6cfcfad2f
coderinblack <kevinlu.email@gmail.com>
2020-10-24 12:21:13 -0700

    Add apollo server setup with hello test resolver

--- packages/server/dist/index.js
+++ packages/server/dist/index.js
@@ -1,2 +1,35 @@
-console.log('Hello world!');
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const apollo_server_express_1 = require("apollo-server-express");
+const express_1 = __importDefault(require("express"));
+const type_graphql_1 = require("type-graphql");
+const constants_1 = require("./constants");
+const HelloResolver_1 = require("./resolvers/HelloResolver");
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const app = express_1.default();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({
+            resolvers: [HelloResolver_1.HelloResolver],
+            validate: false,
+        }),
+        context: ({ req, res }) => ({ req, res }),
+    });
+    apolloServer.applyMiddleware({ app, cors: false });
+    app.get('/', (_, res) => res.redirect('/graphql'));
+    app.listen(constants_1.port, () => console.log(`Listening on port ${constants_1.port}`));
+});
+main().catch((err) => console.error(err));
 //# sourceMappingURL=index.js.map
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello world!');
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
+import { port } from './constants';
+import { HelloResolver } from './resolvers/HelloResolver';
+
+const main = async () => {
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.get('/', (_, res) => res.redirect('/graphql'));
+
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+};
+
+main().catch((err) => console.error(err));
