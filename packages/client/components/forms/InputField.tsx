ea79bd467ba65e42f074b97185f2f8a9b4347e88
coderinblack <kevinlu.email@gmail.com>
2020-10-24 12:04:34 -0700

    :tada: Formik and Yup error handling in InputField

--- packages/client/components/forms/InputField.tsx
+++ packages/client/components/forms/InputField.tsx
@@ -6,20 +6,28 @@ import React, { InputHTMLAttributes } from 'react';
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
   name: string;
+  wrapper?: string;
+  labelStyles?: string;
   textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+  wrapper,
   textarea,
+  labelStyles,
   size: _,
   children,
   ...props
 }) => {
-  const [field, { error }, helpers] = useField(props);
+  const [field, { error }] = useField(props);
+  const hasError = !!error;
   return (
-    <div>
-      <label htmlFor={field.name} className="font-semibold text-gray-700 mb-1">
+    <div className={wrapper || undefined}>
+      <label
+        htmlFor={field.name}
+        className={labelStyles || 'font-semibold text-gray-700 mb-1'}
+      >
         {label}
       </label>
       <input
@@ -27,12 +35,14 @@ export const InputField: React.FC<InputFieldProps> = ({
         {...props}
         id={field.name}
         className={classNames(
-          'form-input w-full transition ease duration-200',
-          { 'border-2 border-red-400': error }
+          props.className
+            ? props.className
+            : 'form-input w-full transition ease duration-200 shadow-sm',
+          { 'border-2 border-red-200 text-red-600': hasError }
         )}
       />
-      {error ? (
-        <p className="flex items-center text-red-400 mt-1">
+      {hasError ? (
+        <p className="flex items-center text-red-500 mt-1">
           <ExclamationCircle size={18} className="mr-1" />
           {error}
         </p>
--- packages/client/pages/register.tsx
+++ packages/client/pages/register.tsx
@@ -1,9 +1,27 @@
-import { Form, Formik } from 'formik';
-import { LockClosed } from 'heroicons-react';
 import React from 'react';
+import * as Yup from 'yup';
+import { Form, Formik, validateYupSchema, yupToFormErrors } from 'formik';
+import { LockClosed } from 'heroicons-react';
 import { InputField } from '../components/forms/InputField';
 import { Banner } from '../components/shared/navigation/banner';
 import { Navbar } from '../components/shared/navigation/navbar';
+import { NextLink } from '../components/shared/nextlink';
+
+const registerSchema = Yup.object().shape({
+  name: Yup.string()
+    .max(255, 'Full Name exceed the character limit')
+    .required('Full name is required'),
+  email: Yup.string()
+    .email()
+    .max(255, 'Email Address exceed the character limit')
+    .required('Email Address  is required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required'),
+  passwordConfirmation: Yup.string()
+    .oneOf([Yup.ref('password')], 'Passwords must match')
+    .required('Please confirm your password'),
+});
 
 const Register: React.FC = () => {
   return (
@@ -12,21 +30,31 @@ const Register: React.FC = () => {
       <Navbar />
       <div className="flex items-center max-w-lg mx-auto px-5">
         <div className="mx-auto py-20">
-          <h1 className="font-extrabold text-4xl">Register Pluto Account</h1>
+          <h1 className="font-extrabold text-4xl text-gray-800">
+            Register Pluto Account
+          </h1>
           <p className="text-gray-600 text-xl">
             Already have an account?{' '}
-            <a href="#" className="text-indigo-500">
+            <NextLink href="/login" className="text-indigo-500 hover:underline">
               Sign in
-            </a>
+            </NextLink>
           </p>
           <Formik
             initialValues={{
               name: '',
               email: '',
               password: '',
-              confirmPassword: '',
+              passwordConfirmation: '',
+              schoolAccount: false,
             }}
-            onSubmit={() => {}}
+            onSubmit={async (values, { setErrors }) => {
+              console.log(values);
+              const errors = await registerSchema.validate(values);
+              setErrors(yupToFormErrors(errors));
+            }}
+            validateOnChange={false}
+            validateOnBlur={false}
+            validationSchema={registerSchema}
           >
             {() => (
               <Form className="mt-8 space-y-5">
@@ -38,34 +66,25 @@ const Register: React.FC = () => {
                     <InputField name="name" label="Full Name" />
                   </div>
                 </div>
-                <div>
-                  <InputField
-                    name="password"
-                    type="password"
-                    label="Password"
-                  />
-                </div>
-                <div>
-                  <InputField
-                    type="password"
-                    name="confirmPassword"
-                    label="Confirm Password"
-                  />
-                </div>
+                <InputField name="password" type="password" label="Password" />
+                <InputField
+                  type="password"
+                  name="passwordConfirmation"
+                  label="Confirm Password"
+                />
                 <div className="flex items-center">
-                  <input
-                    type="checkbox"
+                  <InputField
                     name="school"
-                    id="school"
-                    className="form-checkbox transition ease duration-200"
+                    type="checkbox"
+                    wrapper="flex items-center flex-row-reverse"
+                    className="form-checkbox transition ease duration-200 mr-2"
+                    labelStyles="text-gray-700"
+                    label="School Account?"
                   />
-                  <label htmlFor="school" className="ml-2 text-gray-700">
-                    School Account?
-                  </label>
                 </div>
                 <button
                   type="submit"
-                  className="focus:outline-none focus:shadow-outline transition ease duration-200 flex items-center bg-indigo-500 w-full p-2 rounded-md text-white font-bold"
+                  className="focus:outline-none focus:shadow-outline transition ease duration-200 flex items-center bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-600 w-full p-2 rounded-md text-white font-bold"
                 >
                   <div className="text-indigo-100">
                     <LockClosed size={18} />
